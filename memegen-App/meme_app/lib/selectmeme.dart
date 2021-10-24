2d07d61caf56badb31a00e38adf22bb723540e80
tyNAKAMOL <69255127+tyNAKAMOL@users.noreply.github.com>
2021-10-24 10:41:08 +0700

    new

---
+++ memegen-App/meme_app/lib/selectmeme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SelectMeme extends StatefulWidget {
+  const SelectMeme({Key? key}) : super(key: key);
+
+  @override
+  _SelectMemeState createState() => _SelectMemeState();
+}
+
+class _SelectMemeState extends State<SelectMeme> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- memegen/android/app/src/main/kotlin/com/example/memegen/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.memegen
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
